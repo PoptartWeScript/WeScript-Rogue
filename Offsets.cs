9756d0ce5454f634470c25f20f3b94f968f8faa6
Poptart <70210614+PoptartWeScript@users.noreply.github.com>
2022-03-08 08:32:26 -0600

    Update Offsets.cs

--- Offsets.cs
+++ Offsets.cs
@@ -1,67 +1,67 @@
-﻿using System;
+using System;
 namespace RogueCompany
 {
 
-    public class Offsets
+public class Offsets
 
-    {
-        public static Int64 UWorld = 0x6A0ADF8;
-        public static Int64 GNames = 0x689C100;
+{
+   public static Int64 UWorld = 0x6AA2498;
+   public static Int64 GNames = 0x69334C0;
 
-        public class UE
-        {
+public class UE
+{
 
-            public class UWorld
-            {
-                public static Int64 ULevel = 0x30;
-                public static Int64 OwningGameInstance = 0x188;
-            }
+   public class UWorld
+{
+       public static Int64 ULevel = 0x30;
+       public static Int64 OwningGameInstance = 0x188;
+}
 
-            public class ULevel
-            {
-                public static Int64 AActors = 0x98;
-                public static Int64 AActorsCount = 0xA0;
-            }
+   public class ULevel
+{
+       public static Int64 AActors = 0x98;
+       public static Int64 AActorsCount = 0xA0;
+}
 
-            public class UGameInstance
-            {
-                public static Int64 LocalPlayers = 0x38;
-            }
+   public class UGameInstance
+{
+       public static Int64 LocalPlayers = 0x38;
+}
 
-            public class UPlayer
-            {
-                public static Int64 PlayerController = 0x30;
-            }
+   public class UPlayer
+{
+       public static Int64 PlayerController = 0x30;
+}
 
-            public class APlayerController
-            {
-                public static Int64 AcknowledgedPawn = 0x2A0;
-                public static Int64 PlayerCameraManager = 0x2B8;
-            }
+   public class APlayerController
+{
+       public static Int64 AcknowledgedPawn = 0x2A0;
+       public static Int64 PlayerCameraManager = 0x2B8;
+}
 
-            public class APawn
-            {
-                public static Int64 PlayerState = 0x240;
-            }
-            public class AActor
-            {
-                public static Int64 USceneComponent = 0x130;
-                public static Int64 tempVec = 0x11C;
-                public static Int64 Health = 0x528;
-                public static Int64 CurrentActorHPMax = 0x2894;
-                public static Int64 actor_pawn = 0x118;
-                public static Int64 bKickbackEnableds = 0xAF8;
-                public static Int64 LAKSTeamState = 0x398;
-                public static Int64 LTeamNum = 0x220;
-                public static Int64 AKSTeamState = 0x398;
-                public static Int64 TeamNum = 0x220;
-            }
+   public class APawn
+{
+       public static Int64 PlayerState = 0x240;
+}
+   public class AActor
+{
+       public static Int64 USceneComponent = 0x130;
+       public static Int64 tempVec= 0x11C;
+       public static Int64 Health = 0x538;
+       public static Int64 CurrentActorHPMax = 0x28AC;
+       public static Int64 actor_pawn = 0x118;
+       public static Int64 bKickbackEnableds = 0xAF8;
+       public static Int64 LAKSTeamState = 0x398;
+       public static Int64 LTeamNum = 0x220;
+       public static Int64 AKSTeamState = 0x398;
+       public static Int64 TeamNum = 0x220;
+}
 
-            public class APlayerCameraManager
-            {
-                public static Int64 CameraCachePrivate = 0x1AB0;
-            }
+   public class APlayerCameraManager
+{
+       public static Int64 CameraCachePrivate = 0x1AB0;
+}
 
-        }
-    }
+}
+}
 }
