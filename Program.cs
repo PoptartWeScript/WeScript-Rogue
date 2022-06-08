630325d93c4b373bf13871faaa555dffa122a4dc
Poptart <70210614+PoptartWeScript@users.noreply.github.com>
2022-06-08 09:40:33 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -219,12 +219,12 @@ namespace RogueCompany
                         {
                             if (GWorldPtr == IntPtr.Zero)
                             {
-                                GWorldPtr = Memory.ZwReadPointer(processHandle, GameBase + 0x6B8E488, isWow64Process);
+                                GWorldPtr = Memory.ZwReadPointer(processHandle, GameBase + 0x6DEE1B8, isWow64Process);
                             }
 
                             if (GNamesPtr == IntPtr.Zero)
                             {
-                                GNamesPtr = GameBase + 0x6A1F480;
+                                GNamesPtr = GameBase + 0x6C7F180;
                             }
                         }
                     }
